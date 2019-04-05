bbe9c0238ea2634fe3ea2c631bbdbe12408be088
akash2237778 <akash2237778@gmail.com>
2019-04-06 01:30:14 +0530

    get user data problem in phoneNo check!!!!

--- app/src/main/java/com/example/sahyog/MainActivity.java
+++ app/src/main/java/com/example/sahyog/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sahyog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -10,11 +12,57 @@ import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nameText;
+    EditText userNameText;
+    EditText passwordText;
+    EditText reEnterPassword;
+    EditText phoneNoText;
+    String name;
+    String userName;
+    String password ;
+    String reEnterPass;
+    int phoneNo;
+
+
+
+
+
+public void onClickSignUp (View view){
+    name = String.valueOf(nameText.getText());
+    userName = String.valueOf(userNameText.getText());
+    password = String.valueOf(passwordText.getText());
+    reEnterPass = String.valueOf(reEnterPassword.getText());
+    phoneNo = Integer.parseInt(String.valueOf(phoneNoText.getText()));
+
+    if(name == null || userName == null || password == null || reEnterPassword==null /*|| phoneNo == "null" */){
+        Toast.makeText(MainActivity.this, "Please fill all the Fields !", Toast.LENGTH_SHORT).show();
+
+    }else {
+        if (password.equals(reEnterPass)) {
+            Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Passwords not same !", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+}
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameText = (EditText)findViewById(R.id.nameText);
+        userNameText = (EditText)findViewById(R.id.userNameText);
+        passwordText = (EditText)findViewById(R.id.passwordText);
+        reEnterPassword = (EditText)findViewById(R.id.reEnterPassword);
+        phoneNoText = (EditText)findViewById(R.id.phoneNoText);
+
+
+
+
 
         ParseUser user = new ParseUser();
         user.setUsername("Akash");
