79b3f9f80f6582856a2b2b21d2d76092c87135d3
akash2237778 <akash2237778@gmail.com>
2019-04-06 00:45:19 +0530

    working parseServer

--- app/src/main/java/com/example/sahyog/MainActivity.java
+++ app/src/main/java/com/example/sahyog/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.sahyog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParseUser user = new ParseUser();
+        user.setUsername("Akash");
+        user.setPassword("123456");
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if( e==null ){
+                    Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "unSuccessful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/sahyog/ParseServerDetails.java
@@ -0,0 +1,31 @@
+package com.example.sahyog;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+public class ParseServerDetails extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("697c212c3a04ab63611a1a29261feec124a4c511")
+                // if defined
+                .clientKey("6a738f5d8d76b60e42fba96a3f315aec6e708821")
+                .server("http://13.233.108.240:80/parse")
+                .build()
+        );
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
