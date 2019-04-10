6adbb251188660414ff956e4ca4b6a95ab05e762
akash2237778 <akash2237778@gmail.com>
2019-04-10 22:22:45 +0530

    getting user location

--- app/src/main/java/com/example/sahyog/ProvideService.java
+++ app/src/main/java/com/example/sahyog/ProvideService.java
@@ -1,15 +1,32 @@
 package com.example.sahyog;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class ProvideService extends AppCompatActivity {
     EditText ET_service;
@@ -19,15 +36,41 @@ public class ProvideService extends AppCompatActivity {
     EditText ET_maxweight;
     String pro_username,pro_phone,pro_service, pro_peraddress, pro_curloc, pro_range,pro_maxweight;
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+    TextView textViewAddress;
+    String address = "Address not found !";
 
 
 
-    public void getProLoc(View view)
-    {
-
-
-        ET_curloc.setText("");
+    public void setTextViewInfo( TextView textView ,String toBeTyped){
+        textView.setText(toBeTyped);
     }
+
+    public void updateLocationInfo(Location location){
+        Log.i("info", location.toString());
+
+        Toast.makeText(ProvideService.this, address, Toast.LENGTH_SHORT).show();
+    }
+    public void startListening(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, locationListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startListening();
+        }
+    }
+    public void getProLoc(View view) {
+        setTextViewInfo(textViewAddress,address);
+
+
+    }
+
 
     public void proFormSubmit(View view)
     {
@@ -40,16 +83,17 @@ public class ProvideService extends AppCompatActivity {
 
         pro_username= String.valueOf(ParseUser.getCurrentUser().getUsername());
 
-        provider.put("username","pro_username");
-        provider.put("service","pro_service");
-        provider.put("PerAddress","pro_peraddress");
-        provider.put("CurLocation","pro_curloc");
-        provider.put("ServiceRange","pro_range");
-        provider.put("MaxWeight","pro_maxweight");
+        provider.put("username",pro_username);
+        provider.put("service",pro_service);
+        provider.put("PerAddress",pro_peraddress);
+        provider.put("CurLocation",pro_curloc);
+        provider.put("ServiceRange",pro_range);
+        provider.put("MaxWeight",pro_maxweight);
         provider.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
                 if( e==null ){
+                    Toast.makeText(ProvideService.this, "Successful", Toast.LENGTH_SHORT).show();
                     Log.i("Submit ","Successful");
                 }else{
                     Log.i("Submit ","unSuccessful :" + e.getMessage());
@@ -67,9 +111,62 @@ public class ProvideService extends AppCompatActivity {
 
          ET_service=findViewById(R.id.txtservice);
          ET_peraddress=findViewById(R.id.txtadd);
-         ET_curloc=findViewById(R.id.txtcurlocaion);
+        // ET_curloc=findViewById(R.id.txtcurlocaion);
          ET_range=findViewById(R.id.txtrange);
          ET_maxweight=findViewById(R.id.txtweight);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+        textViewAddress = (TextView)findViewById(R.id.txtcurlocaion) ;
+
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                updateLocationInfo(location);
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listOfGeocoder  = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                    address = listOfGeocoder.get(0).getAddressLine(0);
+                    Log.i("info " , " geocoder successful " + address);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // setTextViewInfo(textViewLatitude, String.valueOf(location.getLatitude()));
+               // setTextViewInfo(textViewLongitude,String.valueOf(location.getLongitude()));
+               // setTextViewInfo(textViewSpeed,String.valueOf(location.getSpeed())+" meters/sec");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+        else {
+
+            startListening();
+            Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            updateLocationInfo(lastLocation);
+
+        }
 
 
     }
